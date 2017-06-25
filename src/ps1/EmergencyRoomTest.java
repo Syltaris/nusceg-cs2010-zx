b47f5560c2827565f44489f0e457adbe35b08c0f
zexuan <syltaris@gmail.com>
2017-06-25 16:02:50 +0800

    implement skeletal tests

--- src/ps1/EmergencyRoomTest.java
+++ src/ps1/EmergencyRoomTest.java
@@ -1,12 +1,31 @@
+package ps1;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class EmergencyRoomTest {
 
+	public static final int MAX_HEAP_SIZE = 100;
+	ERPatient_MaxPriorityQueue patientQueue = new ERPatient_MaxPriorityQueue(MAX_HEAP_SIZE);
+	
 	@Test
-	public void test() {
+	public void shiftUpTests() {
+		patientQueue.insert(new ERPatient("FIRST PATIENT", 30));
+		patientQueue.insert(new ERPatient("SECOND PATIENT", 60));
+		patientQueue.insert(new ERPatient("THIRD PATIENT", 100));
+
+	}
+	
+	@Test
+	public void shiftUp_mustShiftHigherElementUpCorrectly() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void shiftUp_mustNotShiftLowerElementUp() {
 		fail("Not yet implemented");
 	}
 
+	
+	//TODO: test ERPatient_MaxPriorityQueue functions
 }
