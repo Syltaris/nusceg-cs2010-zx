b08cd6ad8acb96a6c3a293d302e0b482696cb4d7
zexuan <syltaris@gmail.com>
2017-07-02 17:34:21 +0800

    PS2A: implement insert() func

--- src/ps2/PatientNames.java
+++ src/ps2/PatientNames.java
@@ -14,7 +14,7 @@ class PatientNames {
 	// is accessible to all methods in this class
 
 	// --------------------------------------------
-	
+
 	// --------------------------------------------
 
 	public PatientNames() {
@@ -64,7 +64,8 @@ class PatientNames {
 	}
 
 	void run() throws Exception {
-		// do not alter this method to avoid unnecessary errors with the automated
+		// do not alter this method to avoid unnecessary errors with the
+		// automated
 		// judging
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
@@ -86,63 +87,127 @@ class PatientNames {
 	}
 
 	public static void main(String[] args) throws Exception {
-		// do not alter this method to avoid unnecessary errors with the automated
+		// do not alter this method to avoid unnecessary errors with the
+		// automated
 		// judging
 		PatientNames ps2 = new PatientNames();
 		ps2.run();
 	}
 }
 
-
-
-class Name_BSTVertex {
+class Name_BSTVertex implements Comparable<Name_BSTVertex> {
 	private Name_BSTVertex parent, left, right;
 	private String key_name;
 	private int height;
 	private int size;
-	
-	public Name_BSTVertex(Name_BSTVertex parent, Name_BSTVertex left, Name_BSTVertex right) {
-		this.parent = parent;
-		this.left = left;
-		this.right = right;
+
+	public Name_BSTVertex(String key_name) {
+		this.key_name = key_name;
 	}
-	
-	//getter/setter methods
-	private Name_BSTVertex getParent() {
+
+	// getter/setter methods
+	public Name_BSTVertex getParent() {
 		return this.parent;
 	}
-	private Name_BSTVertex getLeft() {
+
+	public Name_BSTVertex getLeft() {
 		return this.left;
-	}	
-	private Name_BSTVertex getRight() {
+	}
+
+	public Name_BSTVertex getRight() {
 		return this.right;
 	}
-	private int getHeight() {
+
+	public int getHeight() {
 		return this.height;
 	}
-	private int getSize() {
+
+	public int getSize() {
 		return this.size;
 	}
-	
-	private void setParent(Name_BSTVertex parent) {
+
+	public void setParent(Name_BSTVertex parent) {
 		this.parent = parent;
 	}
-	private void setLeft(Name_BSTVertex left) {
+
+	public void setLeft(Name_BSTVertex left) {
 		this.left = left;
 	}
-	private void setRight(Name_BSTVertex right) {
+
+	public void setRight(Name_BSTVertex right) {
 		this.right = right;
 	}
-	private void setHeight(int height) {
+
+	public void setHeight(int height) {
 		this.height = height;
 	}
-	private void setSize(int size) {
+
+	public void setSize(int size) {
 		this.size = size;
+	}
+
+	@Override
+	public int compareTo(Name_BSTVertex o) {
+		return this.key_name.compareTo(o.key_name);
 	}
 }
 
 class Name_BST {
 	private Name_BSTVertex root;
+
+	public Name_BST() {
+		this.root = null;
+	}
+
+	public void insert(Name_BSTVertex node) {
+		if(this.root == null) {
+			root = node;
+		} else {
+			//traverse left, else right
+			Name_BSTVertex vertex = null, vertex_parent = null;
+			
+			if(this.root.compareTo(node) < 0) {
+				vertex = this.root.getLeft();
+			} else if(this.root.compareTo(node) > 0) {
+				vertex = this.root.getRight();
+			} else {
+				System.out.println("EQUALS?"); //ERR
+			}
+			
+			//find insertion point
+			while(vertex != null) {
+				//if node value is larger than curr vertex, continue going right, else go left
+				if(node.compareTo(vertex) < 0) {
+					vertex_parent = vertex;
+					vertex = vertex.getRight();
+				} else if(node.compareTo(vertex) > 0) {
+					vertex_parent = vertex;
+					vertex = vertex.getLeft();
+				}
+			}
+			
+			vertex = node;
+			
+			//insert node
+			if(vertex_parent.compareTo(node) < 0) {
+				vertex_parent.setRight(node);
+			} else {
+				vertex_parent.setLeft(node);
+			}
+			
+			//init new vertex's links
+			vertex.setParent(vertex_parent);
+			vertex.setLeft(null);
+			vertex.setRight(null);
+		}
+	}
 	
-	
+	@Override
+	public String toString() {
+		String output = new String();
+		
+		//do inorder traversal
+		
+		return output;
+	}
 }
