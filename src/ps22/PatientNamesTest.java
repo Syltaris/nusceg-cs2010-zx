a7242e552267cc1b1f35b2c8a87b4b59af11a02b
zexuan <syltaris@gmail.com>
2017-07-03 02:44:36 +0800

    PS2A: added test correctness validation

--- src/ps22/PatientNamesTest.java
+++ src/ps22/PatientNamesTest.java
@@ -5,12 +5,14 @@ import static org.junit.Assert.*;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.util.StringTokenizer;
+import java.util.TreeSet;
 
 import org.junit.Test;
 
@@ -20,7 +22,6 @@ public class PatientNamesTest {
 	
 	File testFile = new File("D:\\Repos\\cs2010-zx\\src\\ps22\\test1.in");
 	File outputFile = new File("D:\\Repos\\cs2010-zx\\src\\ps22\\test1.out");
-
 	
 	@Test
 	public void mainFileTest() throws IOException {
@@ -55,4 +56,88 @@ public class PatientNamesTest {
 		pr.close();
 	}
 
+	
+	@Test
+	public void fileInputValidTest() throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(testFile));
+		int maxL = 0;
+	    int N = 0, Q = 0;
+	    TreeSet names = new TreeSet();
+	    while (true) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int command = Integer.parseInt(st.nextToken());
+	      if (command == 0) // end of input
+	        break;
+	      else if (command == 1) { // AddPatient
+	        String name = st.nextToken();
+	        int gender = Integer.parseInt(st.nextToken());
+	        if (name.length() > 30) {
+	          System.out.println("LONGER THAN 30 CHARS: " + name);
+	          fail();
+	          return;
+	        }
+	        if (gender != 1 && gender != 2) {
+	          System.out.println("ERROR, GENDER MUST BE 1 OR 2");
+	          fail();
+	          return;
+	        }
+	        if (names.contains(name)) { // make it unique
+	          System.out.println("NOT UNIQUE: " + name + " " + gender);
+	          fail();
+	          return;
+	        }
+	        names.add(name);
+	        N++;
+	        if (N > 20000) {
+	          System.out.println("ERROR, TOO MANY patient names");
+	          fail();
+	          return;
+	        }
+	      }
+	      else if (command == 2) { // RemovePatient
+		        String name = st.nextToken();
+	        if (name.length() > 30) {
+	          System.out.println("LONGER THAN 30 CHARS: " + name);
+	          fail();
+	          return;
+	        }
+	        if (!names.contains(name)) { // name has to be added before
+	          System.out.println("NOT IN THE DATA STRUCTURE BEFORE: " + name);
+	          fail();
+	          return;
+	        }
+	        names.remove(name);
+	        N--;
+	      }
+	      else { // if (command == 3) // Query
+	        String START = st.nextToken();
+	        String END = st.nextToken();
+	        if (START.length() > 30 || END.length() > 30) {
+	          System.out.println("LONGER THAN 30 CHARS: " + START + ", " + END);
+	          fail();
+	          return;
+	        }
+	        if (START.compareTo(END) >= 0) {
+	          System.out.println("ERROR, START MUST BE < END");
+	          fail();
+	          return;
+	        }
+	        int gender = Integer.parseInt(st.nextToken());
+	        if (gender < 0 || gender > 2) {
+	          System.out.println("ERROR, GENDER MUST BE 0, 1, OR 2");
+	          fail();
+	          return;
+	        }
+	        Q++;
+	      }
+	    }
+
+	    if (Q > 20000) {
+	      System.out.println("ERROR, TOO MANY QUERIES");
+	      fail();
+	      return;
+	    }
+
+	    System.out.println("Test data is valid"); 
+	}
 }
