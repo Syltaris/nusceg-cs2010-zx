c3e2122640da327c2be667479c49ba4bfa4d9447
zexuan <syltaris@gmail.com>
2017-07-20 09:20:26 +0800

    PS5: init

---
+++ src/ps5/BleedingVerifierTest.java
@@ -0,0 +1,14 @@
+package ps5;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BleedingVerifierTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
